cc27aac6e0508924cfa2a3693c80aaf21c78b386
Adrianaaymerich <aaymerich@wbinnova.com>
2017-06-06 14:18:29 -0400

    Vistas index y login listas

--- ProyectoSIGNDVC/ProyectoSIGNDVC/Models/Pago.cs
+++ ProyectoSIGNDVC/ProyectoSIGNDVC/Models/Pago.cs
@@ -3,14 +3,21 @@ using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.ComponentModel;
+using System.Data.Entity;
 
 namespace ProyectoSIGNDVC
 {
     public class Pago
     {
+        public int ID { get; set; }
         private int numero_ref;
         private DateTime f_pago;
         private int monto;
         private Boolean aprobado;     
+    }
+
+    public class PagoDBContext : DbContext
+    {
+        public DbSet<Pago> Pagos { get; set; }
     }
 }
