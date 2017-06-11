8d5b9fe6f8d2592fbb37748b9638711d5838f619
craloz <carlosenriquelozanoperez@hotmail.com>
2017-06-11 12:04:16 -0400

    cualquier vaina

--- ProyectoSIGNDVC/ProyectoSIGNDVC/Controllers/ConfigurationController.cs
+++ ProyectoSIGNDVC/ProyectoSIGNDVC/Controllers/ConfigurationController.cs
@@ -20,6 +20,19 @@ namespace ProyectoSIGNDVC.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult RegistroUsuario(String nombre)
+        {
+            ViewBag.Message = "Your application description page."+nombre;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarUsuario()
+        {
+            return View();
+        }
+
         public ActionResult TablaUsuarios()
         {
             ViewBag.Message = "Your application description page.";
